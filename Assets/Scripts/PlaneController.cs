d8df390c9b6e389aaa3a44f12a286eb6076f1369
Ethan Bruins <brudut93@gmail.com>
2018-04-13 17:20:56 +0200

    started a mess

---
+++ Assets/Scripts/PlaneController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//Player controlled in each of the scenes
+public class PlaneController : MonoBehaviour
+{
+
+    public float rotationalSpeed;
+
+    Rigidbody rigid;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        RotateMyBoi();
+    }
+
+    void RotateMyBoi()
+    {
+        Vector3 rotation = new Vector3(Input.GetAxis("Pitch"), Input.GetAxis("Yaw"), Input.GetAxis("Roll"));
+
+        rigid.AddRelativeTorque(rotation * rotationalSpeed);
+    }
+
+}
